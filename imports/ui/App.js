4a51702dd6f4b5681986bbc20f6026d9ecb31b61
Jon Urry <jon@urry.me>
2018-10-09 17:48:10 +0100

    add form for creating a space

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { withTracker } from 'meteor/react-meteor-data';
 
 import { Spaces } from '../api/spaces.js';
@@ -6,9 +7,23 @@ import Space from './Space.js';
 
 // App component - represents the whole app
 class App extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // Find the name field via the React ref
+    const name = ReactDOM.findDOMNode(this.refs.nameInput).value.trim();
+
+    Spaces.insert({
+      name,
+      createdAt: new Date() // current time
+    });
+
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.nameInput).value = '';
+  }
 
   renderSpaces() {
-    return this.props.spaces.map((space) => (
+    return this.props.spaces.map(space => (
       <Space key={space._id} space={space} />
     ));
   }
@@ -19,7 +34,13 @@ class App extends Component {
         <header>
           <h1>Spaces</h1>
         </header>
-
+        <form className="new-name" onSubmit={this.handleSubmit.bind(this)}>
+          <input
+            type="text"
+            ref="nameInput"
+            placeholder="Type to add new name"
+          />
+        </form>
         <ul>{this.renderSpaces()}</ul>
       </div>
     );
@@ -28,6 +49,6 @@ class App extends Component {
 
 export default withTracker(() => {
   return {
-    spaces: Spaces.find({}).fetch(),
+    spaces: Spaces.find({}, { sort: { createdAt: -1 } }).fetch()
   };
 })(App);
