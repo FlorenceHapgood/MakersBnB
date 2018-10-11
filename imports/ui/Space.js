a2dc5a77c0e41adace38bb85cd9e8e959b0e5c30
Jon Urry <jon@urry.me>
2018-10-11 12:05:45 +0100

    Add classes and ids to Space so that tests can locate UI elements

--- imports/ui/Space.js
+++ imports/ui/Space.js
@@ -4,32 +4,43 @@ import classnames from 'classnames';
 
 import { Tasks } from '../api/spaces.js';
 
-
-
 // Space component - represents a single space for rent
 export default class Space extends Component {
   buttonClicked() {
     // Set the checked property to the opposite of its current value
-    Meteor.call('spaces.setBooked', this.props.space._id, !this.props.space.booked);
+    Meteor.call(
+      'spaces.setBooked',
+      this.props.space._id,
+      !this.props.space.booked
+    );
   }
 
   deleteThisSpace() {
-   Meteor.call('spaces.remove', this.props.space._id);
- }
+    Meteor.call('spaces.remove', this.props.space._id);
+  }
 
   render() {
     return (
-
-        <div className="space">
-          <h1>{this.props.space.username}</h1>
+      <div id={this.props.space._id} className="space">
+        <div>
+          <div className="space-owner">
+            <label htmlFor="owner">Owner:</label>
+            <p name="owner">{this.props.space.username}</p>
+          </div>
+          <h1 class="space-name">{this.props.space.name}</h1>
           <button className="delete" onClick={this.deleteThisSpace.bind(this)}>
             &times;
           </button>
-          <h1>{this.props.space.name}</h1>
-          <h3>{this.props.space.description}</h3>
-          <h4>{this.props.space.price}</h4>
-          <button className="booking" onClick={this.buttonClicked.bind(this)} > Book this space! </button>
         </div>
+        <p class="space-description">{this.props.space.description}</p>
+        <div className="space-price">
+          <label htmlFor="price">Price per night: </label>
+          <span name="price">{this.props.space.price}</span>
+          <button className="booking" onClick={this.buttonClicked.bind(this)}>
+            Book this space!
+          </button>
+        </div>
+      </div>
     );
   }
 }
